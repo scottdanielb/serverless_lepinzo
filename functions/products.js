5dba4a1d03e2fe9fd8079c1d30ed42767e9bf233
Daniel <danielmarroquin5224@gmail.com>
2022-07-15 11:53:25 -0500

    added headers

--- functions/products.js
+++ functions/products.js
@@ -37,6 +37,9 @@ exports.handler = async (event, context, cb) => {
       };
     });
     return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       statusCode: 200,
       body: JSON.stringify(products),
     };
--- functions/single-product.js
+++ functions/single-product.js
@@ -24,6 +24,9 @@ exports.handler = async (event, context, cb) => {
         };
       }
       return {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
         statusCode: 200,
         body: JSON.stringify(product),
       };
